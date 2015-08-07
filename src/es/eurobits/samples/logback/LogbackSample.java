52c64bedba856c9281f6dc223cacf9b9765feec6
Daniel Acedos <acedos@localhost.localdomain>
2015-08-07 14:16:30 +0200

    añadir sift

--- src/es/eurobits/samples/logback/LogbackSample.java
+++ src/es/eurobits/samples/logback/LogbackSample.java
@@ -2,14 +2,31 @@ package es.eurobits.samples.logback;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 public class LogbackSample {
     public static void main(String[] args) {
 
         Logger logger = LoggerFactory.getLogger("es.eurobits.samples.logback");
 
-        for (int i = 0; i < 100000; i++) {
-            logger.debug("Hello world:" + i);
+        for (int i = 0; i < 10; i++) {
+            logger.debug(" - " + System.currentTimeMillis());
+        }
+
+        String siftLabel = "foo";
+
+        MDC.put("siftLabel", siftLabel);
+
+        for (int i = 0; i < 10; i++) {
+            logger.debug(siftLabel + " - " + System.currentTimeMillis());
+        }
+
+        siftLabel = "bar";
+
+        MDC.put("siftLabel", siftLabel);
+
+        for (int i = 0; i < 10; i++) {
+            logger.debug(siftLabel + " - " + System.currentTimeMillis());
         }
 
     }
